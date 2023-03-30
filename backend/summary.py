6ff616580055eab3cd8db7998e21af4431f9c67c
JohanPochampally <johanpochampally12@gmail.com>
2023-03-30 11:06:20 +0530

    Switched to Pegasus summarization model

--- backend/summary.py
+++ backend/summary.py
@@ -7,12 +7,30 @@ def summarize(text:str):
     # load Pegasus tokenizer and model
     tokenizer = PegasusTokenizer.from_pretrained('google/pegasus-large')
     model = PegasusForConditionalGeneration.from_pretrained('google/pegasus-large')
-
+    text = text.replace('.', '.<eos>')
+    text = text.replace('!', '!<eos>')
+    text = text.replace('?', '?<eos>')
+    sentences = text.split('<eos>')
+    max_chunk = 500
+    current_chunk = 0
+    chunks = []
+    for sentence in sentences:
+        if len(chunks) == current_chunk + 1:
+            if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk:
+                chunks[current_chunk].extend(sentence.split(' '))
+            else:
+                current_chunk += 1
+                chunks.append(sentence.split(' '))
+        else:
+            chunks.append(sentence.split(' '))
+    
+    for chunk_id in range(len(chunks)):
+        chunks[chunk_id] = ' '.join(chunks[chunk_id])
     # encode the input text using the tokenizer
-    inputs = tokenizer(text, max_length=1024, truncation=True, padding='longest', return_tensors='pt')
+    inputs = tokenizer(chunks, max_length=1024, truncation=True, padding='longest', return_tensors='pt')
 
     # generate the summary using the Pegasus model
-    summary_ids = model.generate(inputs['input_ids'], max_length=50, num_beams=5, early_stopping=True)
+    summary_ids = model.generate(inputs['input_ids'], max_length=100, num_beams=5, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
     # print the summary
