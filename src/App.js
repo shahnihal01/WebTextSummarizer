c6f269dfaa41c1d5e8961a6bf84d77cddae408b9
Nihal Shah <shahnihal01@gmail.com>
2023-04-15 01:08:31 +0530

    Scraper error handling and UI tweeks

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import axios from 'axios';
 function App() {
   const [txt,setTxt] = useState("");
   const [summary,setSummary] = useState("");
+  const [plain,setPlain] = useState("");
   const [isLoading,setIsLoading] = useState(false);
   const [loaded,setLoaded] = useState(false);
+  const [scrapeErr,setScrapeErr] = useState(false);
+  const [shrink,setShrink] = useState(0);
 
   function isUrl(input) {
     // Regular expression for URL pattern
@@ -30,6 +33,13 @@ function App() {
         }
         const summaryResponse = await axios.get(`http://127.0.0.1:5000/data?id=${res.data.id}`);
         setSummary(summaryResponse.data.summary_text);
+        setPlain(summaryResponse.data.plain_text);
+        const plainLen = summaryResponse.data.plain_text.split(" ").length;
+        const sumLen = summaryResponse.data.summary_text.split(" ").length;
+        setShrink((sumLen/plainLen)*100);
+        if(summaryResponse.data.plain_text==="The URL for the article cannot be scraped. Please enter the text"){
+          setScrapeErr(true);
+        }
       }
       else {
         console.log("input is text")
@@ -41,6 +51,10 @@ function App() {
         }
         const summaryResponse = await axios.get(`http://127.0.0.1:5000/data?id=${res.data.id}`);
         setSummary(summaryResponse.data.summary_text);
+        setPlain(summaryResponse.data.plain_text);
+        const plainLen = summaryResponse.data.plain_text.split(" ").length;
+        const sumLen = summaryResponse.data.summary_text.split(" ").length;
+        setShrink((sumLen/plainLen)*100);
       }
     } catch(err){
       console.log(err);
@@ -61,7 +75,6 @@ function App() {
   };
 
   //Text to Speech
-
   const [speaking, setSpeaking] = useState(false);
 
   const speakText = () => {
@@ -75,18 +88,39 @@ function App() {
     }
   };
 
+  const redToHome =()=>{
+    setIsLoading(false);
+    setLoaded(false);
+    setScrapeErr(false);
+    setTxt("");
+  }
+
   return (
     <div className="App flex flex-col justify-evenly h-screen w-full bg-gradient-to-r from-sky-400 to-blue-500 drop-shadow-lg">
       <p className='text-7xl font-Lobster text-white mt-4'>SummUP</p>
       <div className='flex flex-col justify-center align-middle h-[80%] w-full text-center drop-shadow-lg'>
         {loaded ? (
           <div className='flex flex-col bg-white rounded-3xl mx-auto h-auto w-[85%] max-h-[100%] drop-shadow-lg overflow-auto'>
-            <p className='p-2 text-base text-justify overflow-scroll'>{summary}</p>
-            <div className='flex justify-center'>
-              <button className='flex justify-center bg-gradient-to-r from-sky-400 to-blue-500 mt-2 mb-1 w-40 h-12 rounded-2xl text-center items-center border-blue-500 border-4 hover:border-sky-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 drop-shadow-lg' onClick={speakText} disabled={speaking}>
-                <p className='text-xl text-white font-semibold flex items-center text-center'>{speaking ? "Speaking..." : "Listen"}</p>
-              </button>
+            {scrapeErr?(
+              <div>
+                <p className='p-2 text-xl'>{plain}</p>
+                <div className='flex justify-center'>
+                  <button className='flex justify-center bg-gradient-to-r from-sky-400 to-blue-500 mt-2 mb-1 w-40 h-12 rounded-2xl text-center items-center border-blue-500 border-4 hover:border-sky-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 drop-shadow-lg' onClick={redToHome}>
+                    <p className='text-xl text-white font-semibold flex items-center text-center'>Enter Text</p>
+                  </button>
+                </div>
+              </div>
+            ):(
+              <div>
+              <p className='p-2 text-base text-justify overflow-scroll'>{summary}</p>
+              <p className='p-2 text-slate-600'>Summary Shrinkage {Math.floor(shrink)}%</p>
+              <div className='flex justify-center'>
+                <button className='flex justify-center bg-gradient-to-r from-sky-400 to-blue-500 mt-2 mb-1 w-40 h-12 rounded-2xl text-center items-center border-blue-500 border-4 hover:border-sky-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 drop-shadow-lg' onClick={speakText} disabled={speaking}>
+                  <p className='text-xl text-white font-semibold flex items-center text-center'>{speaking ? "Speaking..." : "Listen"}</p>
+                </button>
+              </div>
             </div>
+            )}
           </div>
         ):(
           <div className='flex flex-col justify-center w-full h-full'>
